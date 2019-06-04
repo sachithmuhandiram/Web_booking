cebc8b1fbc516024dff8ad0662e7200abe4f5463
sachith <sachith@sachith.sachith>
2019-06-04 19:34:18 +0530

    Added simple web server running on localhost:8080

---
+++ src/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main(){
+	listen,err := net.Listen("tcp",":8080")
+
+	if err != nil{
+		log.Panic(err)
+	}
+
+	defer listen.Close()
+
+	for {
+		conn,err := listen.Accept()
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\n Hello\n")
+		fmt.Fprintln(conn,"How are you?")
+		conn.Close()
+	}
+}
